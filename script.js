a6f086feeca3480a5d6a0f28a7250d0c21845a26
Shivani sahu <102249991+SahuShivani03@users.noreply.github.com>
2022-09-19 12:22:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,122 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
+let form = document.getElementById('form');
+let textInput = document.getElementById('textInput');
+let dateInput = document.getElementById('dateInput');
+let textarea = document.getElementById('textarea');
+let msg = document.getElementById('msg');
+let add = document.getElementById('add');
+let tasks = document.getElementById('tasks');
+
+let data = [];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  formValidation();
 });
 
-$(document).ready(function () {
-    $('#example').DataTable();
-});
+// get the data and show on screen
+let showTasks = () => {
+  tasks.innerHTML = '';
+  data.map((item, index) => {
+    return (tasks.innerHTML += `
+      <div id=${index}>
+      <span class="fw-bold">${item.text}</span>
+      <span class="small text-secondary">${item.date}</span>
+      <p>${item.description}</p>
+
+      <span class="options">
+        <!-- edit -->
+        <i
+          class="bi bi-pencil-square"
+          onclick="editTask(this)"
+          data-bs-toggle="modal"
+          data-bs-target="#form"
+        ></i>
+
+        <!-- delete -->
+        <i class="bi bi-trash" onclick="deleteTask(this)"></i>
+      </span>
+    </div>
+      
+      `);
+  });
+
+  resetForm();
+};
+
+let resetForm = () => {
+  textInput.value = '';
+  dateInput.value = '';
+  textarea.value = '';
+};
+
+// posting the data
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(data));
+  showTasks();
+
+  console.log(data);
+};
+
+let formValidation = () => {
+  if (textInput.value === '') {
+    // failure
+    msg.innerHTML = '* task title cannot be blank';
+  } else {
+    // success
+    msg.innerHTML = '';
+    acceptData();
+
+    // close the modal after submission
+    add.setAttribute('data-bs-dismiss', 'modal');
+    add.click();
+
+    // IIFE: Immediately Invoked function Expression
+    (() => {
+      add.setAttribute('data-bs-dismiss', '');
+    })();
+  }
+};
+
+// Delete the task
+
+let deleteTask = (e) => {
+  console.log(e.parentElement.parentElement);
+
+  // removing the element from dom , and removing it from the UI
+  e.parentElement.parentElement.remove();
+
+  // deleting the element from the data array
+  data.splice(e.parentElement.parentElement.id, 1);
+
+  localStorage.setItem('tasks', JSON.stringify(data));
+  console.log('data array after deletion', data);
+};
+
+// let fruits = ['banana', 'cherry', 'apple'];
+// // splice(start index, how many elements you want to delete, item1..... itemn)
+// fruits.splice(1, 2, 'mango');
+// console.log(fruits);
+
+// Editing a task
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+  console.log(selectedTask.children[1]);
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  // delete the previous element which you were editing
+  deleteTask(e);
+};
+
+(() => {
+  data = JSON.parse(localStorage.getItem('tasks')) || [];
+  showTasks();
+})();
