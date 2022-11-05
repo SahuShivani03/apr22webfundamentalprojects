4d0767047490d87aa53151d759e75d8c871d42c0
Shivani sahu <102249991+SahuShivani03@users.noreply.github.com>
2022-11-05 09:34:29 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-function geeksforgeeks() {
-    var x = document.getElementById("menus");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
